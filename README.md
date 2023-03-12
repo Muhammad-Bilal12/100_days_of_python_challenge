4a4eb00e061158c3dd0906ba915d21408d2149c1
Muhammad Bilal <muhammadbilalmnaeem@gmail.com>
2023-03-12 20:29:30 +0500

    Day 2 - Project Tip calculator

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # 100_days_of_python_challenge
 
 ### Day 1
-- print Statement Python
+- Print Statement Python
 
+### Day 2
+- Comments - Escape Sequence - Print Statement Python
+
+### Projects # 1
+- Tip calculator
 
---
+++ tip_calculator.py
@@ -0,0 +1,77 @@
+# Comments
+# Comments which is ignore by python
+# and used to add notes
+
+# print("Hello World")  # print something on console
+
+# ------------------------------------------------------
+# # Variable
+# # Which is used to store data in memory
+
+# print("Muhammad Bilal")
+# print("Muhammad Bilal")
+
+# my_name = "Muhammad Bilal"
+# print(my_name)
+
+# input 👉 used to get data from user
+
+# your_name = input("Enter Your Name here : ")
+# print(your_name)
+
+# *note: We can used + operator to combine two or more string
+
+# print("Your name is "+ your_name)
+
+# Lets Make a program to print sum of two integer
+
+# num1 = input("Enter Your number1 : ")
+# num2 = input("Enter Your number2 : ")
+# we have not recieve a accepted output what  will happen
+
+# in input function, we recieve anything in String Format
+
+# print(int(num1) + int(num2))
+# -----------------------------------------------
+
+# Data Types
+
+# String 👉 to store Text
+# Integer 👉 to store number
+# Float 👉 to store decimal values
+
+# to make specific type of variable we used type conversion mthod
+# str()
+# int()
+# float()
+
+
+# Arithmetic Operator 👉 (+,-,*,/,//,**,% )
+
+# print("addition", 5+5)
+# print("subtration", 15 - 5)
+# print("Multiplication",5*5)
+# print("Division",40/3)
+# print("Integer Division",40//3) #return round off values
+# print("Exponent", 2**3) # to find the power of the function
+# print("Modulo/Remainder",30%4) # to find the remainder
+
+
+# ---------------------------------------------------------- #
+
+# Tip Calculator
+
+food_amount = float(input("Enter Food amount $: "))
+tip_percentage = float(input("Enter tip percentage %: ")) / 100
+tip_amount = food_amount*tip_percentage
+total_amount = food_amount + tip_amount
+print("---------------------------------")
+print(f"🍔 Food amount 👉 ${food_amount}")
+print(f"⚖ Tip amount 👉 ${tip_amount}")
+print("\n")
+print(f"🖨 Total amount 👉 ${total_amount}")
+print("---------------------------------")
+
+
+
+
