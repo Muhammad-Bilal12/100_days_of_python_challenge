149c6b2fc30185e83ed3fe07f2deb712fbcca881
Muhammad Bilal <muhammadbilalmnaeem@gmail.com>
2023-03-16 01:21:32 +0500

    Day 8 - Exercise 8 solution and udate folder structure

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # 100_days_of_python_challenge
 
-### Day 1
+## Day 1 - What is Programming and Python?
+
+
+
+### [Day 1](./DAY1.md "Introduction to Programming")
 - Print Statement Python
 
-### Day 2
+### [Day 2](./# "Amazing Projects")
 - Comments - Escape Sequence - Print Statement Python
 
 ### Projects # 1
---
+++ basic_calculator.py
@@ -0,0 +1,26 @@
+
+
+# Exercise 7
+# Solution
+
+# take Integer value from User 
+n = int(input("Enter Your first number : "))
+m = int(input("Enter Your first number : "))
+
+# Calculate and print the result
+ans1 = n+m
+print("Addition of",n,"and",m,"is", ans1)
+ans2 = n-m
+print("Subtraction of",n,"and",m,"is", ans2)
+ans3 = n*m
+print("Multiplication of",n,"and",m,"is", ans3)
+ans4 = n/m
+print("Division of",n,"and",m,"is", ans4)
+ans5 = n%m
+print("Modulus/Remainder of",n,"and",m,"is", ans5)
+ans6 = n//m
+print("Floor Division of",n,"and",m,"is", ans6)
+ans7 = n**m
+print("Exponent of ",n,"and ",m,"is ", ans7)
+
+# Completed
