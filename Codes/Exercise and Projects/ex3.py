300576b5c04421ae36cdd29568e83e14d251bcc0
Muhammad Bilal <muhammadbilalmnaeem@gmail.com>
2023-03-22 02:31:22 +0500

    100 Days Complete Successfully!

--- Codes/Exercise and Projects/ex3.py
+++ Codes/Exercise and Projects/ex3.py
@@ -2,7 +2,90 @@
 # Kaun bane ga Crore patti
 
 # Question Statement
-# Create a program capable of displaying questions to the user like KBC. 
+# Create a program capable of displaying questions to the user like KBC.
 # Use List data type to store the questions and their correct answers.
 # Display the final amount the person is taking home after playing the game.
+
+
+# Question which ask on KBC
+questions = [
+    [
+        "Which programming language you used to create website",
+        "BhaiLang","Javascript","Python","C",2
+    ],
+    [
+        "Which programming language you used to create website",
+        "BhaiLang","Javascript","Python","C",2
+    ],
+    [
+        "Which programming language you used to create website",
+        "BhaiLang","Javascript","Python","C",2
+    ],
+    [
+        "Which programming language you used to create website",
+        "BhaiLang","Javascript","Python","C",2
+    ],
+    [
+        "Which programming language you used to create website",
+        "BhaiLang","Javascript","Python","C",2
+    ],
+    [
+        "Which programming language you used to create website",
+        "BhaiLang","Javascript","Python","C",2
+    ],
+    [
+        "Which programming language you used to create website",
+        "BhaiLang","Javascript","Python","C",2
+    ],
+    [
+        "Which programming language you used to create website",
+        "BhaiLang","Javascript","Python","C",2
+    ],
+    [
+        "Which programming language you used to create website",
+        "BhaiLang","Javascript","Python","C",2
+    ],
+    [
+        "Which programming language you used to create website",
+        "BhaiLang","Javascript","Python","C",2
+    ], 
+  
+]
+
+# Levels on KBC
+levels = [1000, 2000, 3000, 5000, 10000, 20000, 40000, 80000, 160000, 320000]
+money = 0
+
+# to iterate loop
+for i in range(0,len(questions)):
+    question = questions[i]
+    print(f"\n\n Question for RS {levels[i]}") 
+    print(f"a. {question[1]}          b. {question[2]} ")
+    print(f"c. {question[3]}          d. {question[4]} ")
+    reply =  int(input("Enter your answer (1-4) or  0 to quit:\n" ))
+
+    # Check Answer
+    if reply == 0:
+        money = levels[i-1]
+        break
+    if reply == question[-1]:
+        print(f"Correct answer: {question[-1]}, you have won Rs. {levels[i]}")
+        if(i == 4):
+           money = 10000
+        elif(i == 9):
+            money = 320000
+        elif(i == 14):
+            money = 10000000
+    else:
+        print("Wrong answer!")
+        break 
+
+print(f"Thaks for Playing!!")
+print(f"You have earn money is {money}")
+
+
+
+
+
+
 
---
+++ Codes/modules/regex.py
@@ -0,0 +1,4 @@
+# Regex
+
+import re
+
